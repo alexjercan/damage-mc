214084969033e6c3d1a6625429b58b91d7f59d28
Alex Jercan <jercan_alex27@yahoo.com>
2025-08-18 10:54:37 +0300

    Added nixos dependencies

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A basic flake for my Projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in {
+      packages = {
+          default = {}; # TODO: Add package here
+      };
+
+      apps = {
+        # TODO: Add app here
+        # default = flake-utils.lib.mkApp {
+        #   drv = ...;
+        # };
+      };
+
+      devShells.default = pkgs.mkShell rec {
+        nativeBuildInputs = [];
+
+        buildInputs = with pkgs; [
+          flite
+          glfw
+          gradle
+          jdk21
+          libGL
+          openal
+          alsa-lib
+        ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+
+        shellHook = ''
+          export JAVA_HOME=${pkgs.jdk21}
+        '';
+      };
+    })
+  );
+}
