4456f7682d8ec3333f6f385d93e511084fc056af
Alex Jercan <jercan_alex27@yahoo.com>
2025-08-18 15:14:45 +0300

    feat: implement ISR for non-blocking send

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #define DELAY_MS 100.0
 #define F_CPU 16000000UL
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define BAUD 9600
 #define MY_UBRR F_CPU/16/BAUD-1
+
+volatile uint16_t blink_delay = 500;
+volatile uint16_t counter = 0;
 
 void uart_init(void) {
     unsigned int ubrr = MY_UBRR;
     UBRR0H = (ubrr >> 8);
     UBRR0L = ubrr;
-
     UCSR0B = (1 << RXEN0);
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
@@ -20,27 +22,32 @@ uint8_t uart_read(void) {
     return UDR0;
 }
 
-void delay_variable(uint8_t ms) {
-    for (uint16_t i = 0; i < ms; i++) {
-        _delay_ms(DELAY_MS);
+ISR(TIMER1_COMPA_vect) {
+    counter++;
+    if (counter >= blink_delay) {
+        PORTB ^= (1 << PB5); // toggle LED
+        counter = 0;
     }
+}
+
+void timer1_init(void) {
+    TCCR1B |= (1 << WGM12);
+    OCR1A = 16000;
+    TIMSK1 |= (1 << OCIE1A);
+    TCCR1B |= (1 << CS10);
+    sei();
 }
 
 int main(void) {
     DDRB |= (1 << PB5);
     uart_init();
+    timer1_init();
 
-    uint8_t delay_ms = 10;
     while (1) {
         if (UCSR0A & (1 << RXC0)) {
-            delay_ms = uart_read();
-            if (delay_ms == 0) delay_ms = 1;
+            uint8_t value = uart_read();
+            if (value == 0) value = 1;
+            blink_delay = value * 100;
         }
-
-        PORTB |= (1 << PB5);
-        delay_variable(delay_ms);
-
-        PORTB &= ~(1 << PB5);
-        delay_variable(delay_ms);
     }
 }
